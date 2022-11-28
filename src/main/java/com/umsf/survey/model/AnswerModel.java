224df32db8c77d852516dbc9874c400e632e8318
Vitaliy Babich <85701180+babichv@users.noreply.github.com>
2022-11-28 15:27:50 +0200

    Add method for get stats answer

--- src/main/java/com/umsf/survey/model/AnswerModel.java
+++ src/main/java/com/umsf/survey/model/AnswerModel.java
@@ -2,118 +2,115 @@ package com.umsf.survey.model;
 
 import com.umsf.survey.entity.Answer;
 
+import java.util.Objects;
+
 public class AnswerModel {
     private String subject;
     private String lecturer;
-    private String extra;
-    private Byte answer1;
-    private Byte answer2;
-    private Byte answer3;
-    private Byte answer4;
-    private Byte answer5;
-    private Byte answer6;
-    private Byte answer7;
-    private Byte answer8;
-    private Byte answer9;
-    private Byte answer10;
-    private Byte answer11;
+
+    private double answer1;
+    private double answer2;
+    private double answer3;
+    private double answer4;
+    private double answer5;
+    private double answer6;
+    private double answer7;
+    private double answer8;
+    private double answer9;
+    private double answer10;
+    private double answer11;
     private double avg;
     private int countAnswers;
 
 
     public AnswerModel() {
     }
-    public static AnswerModel entityToModel(Answer answer){
+
+    public static AnswerModel entityToModel(IAnswerCustom answer){
+        AnswerModel answerModel = new AnswerModel();
         if (answer == null){
             throw new NullPointerException();
         }
-        AnswerModel answerModel = new AnswerModel();
         double avg = 0;
-        int countAnswers = 0;
-
-        if (answer.getAnswer1() != null){
+        int count = 0;
+        if (!Objects.isNull(answer.getAnswer1())){
             answerModel.setAnswer1(answer.getAnswer1());
             avg = avg + answer.getAnswer1();
-            countAnswers++;
+            count++;
         }
-        if (answer.getAnswer2() != null){
+        if (!Objects.isNull(answer.getAnswer2())){
             answerModel.setAnswer2(answer.getAnswer2());
             avg = avg + answer.getAnswer2();
-            countAnswers++;
+            count++;
         }
-        if (answer.getAnswer3() != null){
+        if (!Objects.isNull(answer.getAnswer3())){
             answerModel.setAnswer3(answer.getAnswer3());
             avg = avg + answer.getAnswer3();
-            countAnswers++;
+            count++;
         }
-        if (answer.getAnswer4() != null){
+        if (!Objects.isNull(answer.getAnswer4())){
             answerModel.setAnswer4(answer.getAnswer4());
             avg = avg + answer.getAnswer4();
-            countAnswers++;
+            count++;
         }
-        if (answer.getAnswer5() != null){
+        if (!Objects.isNull(answer.getAnswer5())){
             answerModel.setAnswer5(answer.getAnswer5());
             avg = avg + answer.getAnswer5();
-            countAnswers++;
+            count++;
         }
-        if (answer.getAnswer6() != null){
+        if (!Objects.isNull(answer.getAnswer6())){
             answerModel.setAnswer6(answer.getAnswer6());
             avg = avg + answer.getAnswer6();
-            countAnswers++;
+            count++;
         }
-        if (answer.getAnswer7() != null){
+        if (!Objects.isNull(answer.getAnswer7())){
             answerModel.setAnswer7(answer.getAnswer7());
             avg = avg + answer.getAnswer7();
-            countAnswers++;
+            count++;
         }
-        if (answer.getAnswer8() != null){
+        if (!Objects.isNull(answer.getAnswer8())){
             answerModel.setAnswer8(answer.getAnswer8());
             avg = avg + answer.getAnswer8();
-            countAnswers++;
+            count++;
         }
-        if (answer.getAnswer9() != null){
+        if (!Objects.isNull(answer.getAnswer9())){
             answerModel.setAnswer9(answer.getAnswer9());
             avg = avg + answer.getAnswer9();
-            countAnswers++;
+            count++;
         }
-        if (answer.getAnswer10() != null){
+        if (!Objects.isNull(answer.getAnswer10())){
             answerModel.setAnswer10(answer.getAnswer10());
             avg = avg + answer.getAnswer10();
-            countAnswers++;
+            count++;
         }
-        if (answer.getAnswer11() != null){
+        if (!Objects.isNull(answer.getAnswer11())){
             answerModel.setAnswer11(answer.getAnswer11());
             avg = avg + answer.getAnswer11();
-            countAnswers++;
+            count++;
         }
-        
-        answerModel.setAvg(avg/countAnswers);
-        answerModel.setExtra(answer.getExtra());
+        answerModel.setAvg(avg/count);
         answerModel.setLecturer(answer.getLecturer());
-
-
+        answerModel.setSubject(answer.getSubject());
         return answerModel;
     }
 
-    public AnswerModel(String subject,
-                       String lecturer,
-                       String extra,
-                       Byte answer1,
-                       Byte answer2,
-                       Byte answer3,
-                       Byte answer4,
-                       Byte answer5,
-                       Byte answer6,
-                       Byte answer7,
-                       Byte answer8,
-                       Byte answer9,
-                       Byte answer10,
-                       Byte answer11,
+    public AnswerModel(String subject, String lecturer,
+                       double answer1,
+                       double answer2,
+                       double answer3,
+                       double answer4,
+                       double answer5,
+                       double answer6,
+                       double answer7,
+                       double answer8,
+                       double answer9,
+                       double answer10,
+                       double answer11,
                        double avg,
                        int countAnswers) {
+
         this.subject = subject;
         this.lecturer = lecturer;
-        this.extra = extra;
         this.answer1 = answer1;
         this.answer2 = answer2;
         this.answer3 = answer3;
@@ -145,99 +142,91 @@ public class AnswerModel {
         this.lecturer = lecturer;
     }
 
-    public String getExtra() {
-        return extra;
-    }
-
-    public void setExtra(String extra) {
-        this.extra = extra;
-    }
-
-    public Byte getAnswer1() {
+    public double getAnswer1() {
         return answer1;
     }
 
-    public void setAnswer1(Byte answer1) {
+    public void setAnswer1(double answer1) {
         this.answer1 = answer1;
     }
 
-    public Byte getAnswer2() {
+    public double getAnswer2() {
         return answer2;
     }
 
-    public void setAnswer2(Byte answer2) {
+    public void setAnswer2(double answer2) {
         this.answer2 = answer2;
     }
 
-    public Byte getAnswer3() {
+    public double getAnswer3() {
         return answer3;
     }
 
-    public void setAnswer3(Byte answer3) {
+    public void setAnswer3(double answer3) {
         this.answer3 = answer3;
     }
 
-    public Byte getAnswer4() {
+    public double getAnswer4() {
         return answer4;
     }
 
-    public void setAnswer4(Byte answer4) {
+    public void setAnswer4(double answer4) {
         this.answer4 = answer4;
     }
 
-    public Byte getAnswer5() {
+    public double getAnswer5() {
         return answer5;
     }
 
-    public void setAnswer5(Byte answer5) {
+    public void setAnswer5(double answer5) {
         this.answer5 = answer5;
     }
 
-    public Byte getAnswer6() {
+    public double getAnswer6() {
         return answer6;
     }
 
-    public void setAnswer6(Byte answer6) {
+    public void setAnswer6(double answer6) {
         this.answer6 = answer6;
     }
 
-    public Byte getAnswer7() {
+    public double getAnswer7() {
         return answer7;
     }
 
-    public void setAnswer7(Byte answer7) {
+    public void setAnswer7(double answer7) {
         this.answer7 = answer7;
     }
 
-    public Byte getAnswer8() {
+    public double getAnswer8() {
         return answer8;
     }
 
-    public void setAnswer8(Byte answer8) {
+    public void setAnswer8(double answer8) {
         this.answer8 = answer8;
     }
 
-    public Byte getAnswer9() {
+    public double getAnswer9() {
         return answer9;
     }
 
-    public void setAnswer9(Byte answer9) {
+    public void setAnswer9(double answer9) {
         this.answer9 = answer9;
     }
 
-    public Byte getAnswer10() {
+    public double getAnswer10() {
         return answer10;
     }
 
-    public void setAnswer10(Byte answer10) {
+    public void setAnswer10(double answer10) {
         this.answer10 = answer10;
     }
 
-    public Byte getAnswer11() {
+    public double getAnswer11() {
         return answer11;
     }
 
-    public void setAnswer11(Byte answer11) {
+    public void setAnswer11(double answer11) {
         this.answer11 = answer11;
     }
 
---
+++ src/main/java/com/umsf/survey/model/IAnswerCustom.java
@@ -0,0 +1,19 @@
+package com.umsf.survey.model;
+
+public interface IAnswerCustom {
+    long getId();
+    double getAnswer1();
+    double getAnswer2();
+    double getAnswer3();
+    double getAnswer4();
+    double getAnswer5();
+    double getAnswer6();
+    double getAnswer7();
+    double getAnswer8();
+    double getAnswer9();
+    double getAnswer10();
+    double getAnswer11();
+    String getLecturer();
+    String getSubject();
+    int getCount();
+}
