d6a78bf85045a9c3a454e8cbe59fff6e71637b88
DirePredium <user83054@gmail.com>
2022-12-05 20:24:34 +0200

    Commenting spring security

--- src/main/java/com/umsf/survey/config/WebConfig.java
+++ src/main/java/com/umsf/survey/config/WebConfig.java
@@ -11,6 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+//        .allowedOrigins("http://127.0.0.1:5500/").allowedHeaders("*")
+        ;
     }
 }
--- src/main/java/com/umsf/survey/security/SecurityConfig.java
+++ src/main/java/com/umsf/survey/security/SecurityConfig.java
@@ -1,42 +1,42 @@
-package com.umsf.survey.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("user")
-                .authorities("ROLE_USER")
-                .and()
-                .withUser("umsfadmin")
-                .password("umsfadmin")
-                .authorities("ROLE_ADMIN");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/survey/answer/**").hasAuthority("ROLE_ADMIN")
-
-                .antMatchers("/**").permitAll()
-                .and().formLogin();
-    }
-}
+//package com.umsf.survey.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+//
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//        auth.inMemoryAuthentication()
+//                .withUser("user")
+//                .password("user")
+//                .authorities("ROLE_USER")
+//                .and()
+//                .withUser("umsfadmin")
+//                .password("umsfadmin")
+//                .authorities("ROLE_ADMIN");
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/survey/answer/**").hasAuthority("ROLE_ADMIN")
+//
+//                .antMatchers("/**").permitAll()
+//                .and().formLogin();
+//    }
+//}
