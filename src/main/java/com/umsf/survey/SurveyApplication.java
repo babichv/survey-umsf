29ee6bfc7ed6b14da9c1067d663d514b7fab7e49
DirePredium <user83054@gmail.com>
2022-12-07 15:16:13 +0200

    Deploy to heroku, try 3

--- src/main/java/com/umsf/survey/SurveyApplication.java
+++ src/main/java/com/umsf/survey/SurveyApplication.java
@@ -1,13 +1,29 @@
 package com.umsf.survey;
 
+import java.lang.management.ManagementFactory;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class SurveyApplication {
 
 	public static void main(String[] args) {
+		System.getProperty("java.version");
+		System.out.println(ManagementFactory.getRuntimeMXBean().getVmVersion());
 		SpringApplication.run(SurveyApplication.class, args);
 	}
+	
+    @Bean
+    ApplicationRunner applicationRunner(Environment environment) {
+        return args -> {
+        	System.out.println("from application.properties " + environment.getProperty("spring.datasource.username"));
+        	System.out.println("from system.properties " + environment.getProperty("maven.version"));
+        	System.out.println("from system.properties " + environment.getProperty("java.runtime.version"));
+        };
+    }
 
 }
--- src/main/java/com/umsf/survey/config/WebConfig.java
+++ src/main/java/com/umsf/survey/config/WebConfig.java
@@ -1,12 +1,14 @@
 package com.umsf.survey.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@PropertySource("classpath:system.properties")
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
